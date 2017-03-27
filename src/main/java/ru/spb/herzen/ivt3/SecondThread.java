db8b4560ae90db912d54459ce46f664681a662b8
knastin <knastin@users.noreply.github.com>
2017-03-27 17:02:44 +0300

    Update SecondThread.java

--- src/main/java/ru/spb/herzen/ivt3/SecondThread.java
+++ src/main/java/ru/spb/herzen/ivt3/SecondThread.java
@@ -1,3 +1,17 @@
 package spb.herzen;
 public class SecondThreed extends Thread {
    public void run(){
+Thread myThready = new Thread();
+        myThready.start();
+      for(int i = 0; i < 5; i++)
+		{
+         try {
+            myThready.sleep(1000);
+             } catch (InterruptedException e) {
+            e.printStackTrace();
+         break;
+         }
+      }
+   }
+}
+         
